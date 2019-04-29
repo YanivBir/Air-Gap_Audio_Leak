534bfa8f0ab079c4b9cd57e44d39f11934d6c395
YanivBir <yaniv.birka@gmail.com>
2019-04-29 10:58:37 +0300

    Change BSD checksum sign to xor

--- packet/err_detection.py
+++ packet/err_detection.py
@@ -1,15 +1,6 @@
 from packet.packet_constants import *
 from packet.packet import *
 
-def bsd_checksum(pkt):
-    data = pkt.to_byte_array()
-    checksum = 0
-    for b in data:
-        checksum = (checksum&0xff >> 1) + ((checksum&1) << 7)
-        checksum = checksum + b
-        checksum = checksum&0xff
-    return checksum
-
 def calc_checksum(pkt):
     checksum_str = str(bsd_checksum(pkt))
     right = 0
@@ -18,4 +9,13 @@ def calc_checksum(pkt):
     left = 0
     if (checksum_str[:2]!=''):
         left = int(checksum_str[:2])
-    return (right+left)
+    return (right^left)
+
+def bsd_checksum(pkt):
+    data = pkt.to_byte_array()
+    checksum = 0
+    for b in data:
+        checksum = (checksum&0xff >> 1) + ((checksum&1) << 7)
+        checksum = checksum + b
+        checksum = checksum&0xff
+    return checksum
